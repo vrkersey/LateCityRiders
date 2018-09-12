ad27d344486af213b200a5a7f1fd4eca86e6c451
BradyJacobson <u0993342@utah.edu>
2018-09-12 16:34:51 -0600

    Driving mechanics and scene
    Added the simple driving mechanics and a scene to practice in. It will
    change drastically over the next few days.

---
+++ Assets/Scripts/Driving_Controls.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driving_Controls : MonoBehaviour
+{
+
+    public float currentspeed, acceleration, topspeed, bottomspeed;
+    public Rigidbody rb;
+
+	// Update is called once per frame
+	void Update()
+    {   
+        KeyPress();
+      //  transform.Translate(currentspeed, 0, 0);
+	}
+    private void KeyPress()
+    {
+        //Forward acceleration
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) 
+        {
+            rb.AddForce(transform.forward * acceleration);
+            //if (currentspeed <= topspeed)
+            //    currentspeed = currentspeed + iteration;
+            //if (currentspeed > topspeed)
+            //    currentspeed = topspeed;
+        }
+
+        //Reverse/slowing down
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) 
+        {
+            rb.AddForce(-transform.forward * acceleration);
+            //if(currentspeed >= bottomspeed)
+            //{
+            //    currentspeed = currentspeed - iteration;
+            //    if (currentspeed < bottomspeed)
+            //        currentspeed = bottomspeed;
+            //}
+        }
+
+        //Rotate left
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -0.5F, 0);
+        }
+
+        //Rotate Right
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0.5F, 0);
+        }
+
+        //Reverse Direction and instant stop (Debugging)
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            transform.Rotate(0, 180, 0);
+         
+            rb.velocity = new Vector3(0,0,0);
+        }
+
+    }
+}
