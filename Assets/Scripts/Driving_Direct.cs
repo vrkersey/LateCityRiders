32d30c9d074d71151fc0ff0b5eba8a2edfce206f
BradyJacobson <u0993342@utah.edu>
2018-09-18 14:18:08 -0600

    Fixed bugs in direct car
    Fixed bugs where car would not be grounded despite being grounded. Also
    some small control tweaks.

--- Assets/Scripts/Driving_Direct.cs
+++ Assets/Scripts/Driving_Direct.cs
@@ -16,7 +16,7 @@ public class Driving_Direct : MonoBehaviour
         acceleration = 0.3f;
         slowDown = 0.2f;
         maxSpeed = 60f;
-        turnSpeed = 0.5f;
+        turnSpeed = 0.8f;
         grounded = false;
 
     }
@@ -24,8 +24,6 @@ public class Driving_Direct : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //ok = PlayerInCar;
-        //if (playerInCar)
             KeyPress();
         //Fix this TODO:
         transform.position += -transform.forward * speed * Time.deltaTime;
@@ -33,18 +31,22 @@ public class Driving_Direct : MonoBehaviour
 
     void KeyPress()
     {
+        //If the player is grounded, the player can accelerate. The following if statements control acceleration.
         if (grounded == true)
         {
             //Speeds up the vehicle manually. 
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))&&(!Input.GetKey(KeyCode.S) || !Input.GetKey(KeyCode.DownArrow)))
             {
-                speed += acceleration;
+                if (speed < 0)
+                    speed += 2 * acceleration;
+                else
+                    speed += acceleration;
                 if (speed > maxSpeed)
                     speed = maxSpeed;
             }
 
             // Slows down the vehicle manually. Acts as a reverse as well.
-            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) && (!Input.GetKey(KeyCode.W) || !Input.GetKey(KeyCode.UpArrow)))
             {
                 if (speed > 0)
                     speed -= 2 * acceleration;
@@ -52,10 +54,26 @@ public class Driving_Direct : MonoBehaviour
                     speed -= acceleration;
                 if (speed < -maxSpeed)
                     speed = -maxSpeed;
+            }
 
+            //Slows down forward moving vehicle. Sets slowed down car to 0 if necessary.
+            if (speed > 0 && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.UpArrow))
+            {
+                speed -= slowDown;
+                if (speed < 0)
+                    speed = 0;
+            }
+
+            //Slows down reversing vehicle. Sets slowed down car to 0 if necessary.
+            if (speed < 0 && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.UpArrow))
+            {
+                speed += slowDown;
+                if (speed > 0)
+                    speed = 0;
             }
         }
 
+        //The following do not rely on being grounded. These control car direction.
         //Rotate left. Attempted to rotate around back of vehicle.
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
@@ -71,23 +89,10 @@ public class Driving_Direct : MonoBehaviour
             temp.x += 5f;
             transform.RotateAround(temp, transform.up, turnSpeed);
         }
-        //Slows down forward moving vehicle. Sets slowed down car to 0 if necessary.
-        if (speed > 0 && !Input.anyKey)
-        {
-            speed -= slowDown;
-            if (speed < 0)
-                speed = 0;
-        }
-
-        //Slows down reversing vehicle. Sets slowed down car to 0 if necessary.
-        if (speed < 0 && !Input.anyKey)
-        {
-            speed += slowDown;
-            if (speed > 0)
-                speed = 0;
-        }
     }
 
+    //On collison enter may not be necessary... TODO
+    //When driver lands on ground, regain acceleration based control.
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Road"))
@@ -95,6 +100,20 @@ public class Driving_Direct : MonoBehaviour
             grounded = true;
         }
     }
+
+    //When driver is currently on ground, maintain acceleration control.
+    void OnCollisionStay(Collision other)
+    {
+        foreach (ContactPoint contact in other.contacts)
+        {
+            if (other.gameObject.CompareTag("Road"))
+            {
+                grounded = true;
+            }
+        }
+    }
+
+    //When driver detatches from ground, remove driver's ability to accelerate.
     void OnCollisionExit(Collision other)
     {
         if (other.gameObject.CompareTag("Road"))
