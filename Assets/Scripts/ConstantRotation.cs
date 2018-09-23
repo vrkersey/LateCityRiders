05df34bb6e7d098b463bb451500bf5c4a8906551
exploder1010 <nicholascarrow1010@gmail.com>
2018-09-22 19:41:30 -0600

    added background

---
+++ Assets/Scripts/ConstantRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour {
+
+    public float x;
+    public float y;
+    public float z;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(transform.position, transform.up, x);
+        transform.RotateAround(transform.position, transform.right, y);
+        transform.RotateAround(transform.position, transform.forward, z);
+    }
+}
