600c8a85d853d1d69b4479b628e0638bb766fbe0
Victor Kersey <vkersey@axis41.com>
2018-09-25 15:28:02 -0600

    HUD with timer, and script

---
+++ Assets/Scripts/timerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerScript : MonoBehaviour {
+
+    public float startTime;
+    private float timeRemaining;
+    private Text timeDisplay;
+
+    // Use this for initialization
+    void Start()
+    {
+        timeDisplay = GameObject.Find("Time").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeRemaining = startTime > 0 ? startTime - Time.timeSinceLevelLoad : 0;
+        int minutes = (int)timeRemaining / 60;
+        int seconds = (int)timeRemaining % 60;
+
+        string textTime = string.Format("{0:00}   {1:00}", minutes, seconds);
+        timeDisplay.text = textTime;
+    }
+}
