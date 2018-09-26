999b34b07db9c4686d42a5c35f01690bac9dde5f
exploder1010 <nicholascarrow1010@gmail.com>
2018-09-26 13:36:33 -0600

    easter egg for victor

---
+++ Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEgg : MonoBehaviour {
+
+    public GameObject easteregg;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.GetComponent<Driving_Controls>() && other.gameObject.transform.GetComponent<Driving_Controls>().playerInCar)
+        {
+            Debug.Log("win");
+            easteregg.SetActive(true);
+        }
+        else if (other.gameObject.transform.GetComponent<Driving_Controls>() && !other.gameObject.transform.GetComponent<Driving_Controls>().playerInCar)
+        {
+            Destroy(this);
+            Debug.Log("win");
+        }
+    }
+}
